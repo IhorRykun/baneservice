2c509f4df8513ac11683dc43d5c6ef3a62e7107e
IhorRykun <IhorRykun@gmail.com>
2024-04-01 19:35:17 +0300

    add counter js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
-import './js/button_wave';
-import './js/button__more';
-import './js/popap';
-import './js/modal';
-import './js/slick';
-import './js/burger-menu';
+import './js/counter';
--- src/js/popap.js
+++
@@ -1,9 +0,0 @@
-import PhotoSwipeLightbox from '../../node_modules/photoswipe/dist/photoswipe-lightbox.esm';
-
-const lightbox = new PhotoSwipeLightbox({
-  gallery: '#my-gallery',
-  children: 'a',
-  pswpModule: () => import('photoswipe'),
-});
-
-lightbox.init();
