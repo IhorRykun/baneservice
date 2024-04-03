0ada4a1b913e12036d6d636807e01d46899d5f4c
IhorRykun <IhorRykun@gmail.com>
2024-04-03 20:45:08 +0300

    update

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -1,6 +1,18 @@
 const btnBurger = document.querySelector('.nav__span-button');
 const modal = document.querySelector('.nav');
+const btnClose = document.querySelector('.svg--close');
+const body = document.querySelector('body');
 
 btnBurger.addEventListener('click', () => {
   modal.classList.toggle('display-nan');
+  btnClose.classList.toggle('display-nan');
+  btnBurger.classList.toggle('display-nan');
+  body.classList.add('scroll');
+});
+
+btnClose.addEventListener('click', () => {
+  modal.classList.toggle('display-nan');
+  btnClose.classList.toggle('display-nan');
+  btnBurger.classList.toggle('display-nan');
+  body.classList.remove('scroll');
 });
